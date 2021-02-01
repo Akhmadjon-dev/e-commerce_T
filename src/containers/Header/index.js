b4e74dedd03636e6acdfc1f0146db09a0ceb5187
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-02-02 00:59:04 +0500

    initial setup

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -3,25 +3,16 @@ import S from "../../styles/pages/header";
 
 export default class Header extends Component {
   render() {
-    return <S.Header>
-        <div className="header__logo">
-            Logo
-        </div>
+    return (
+      <S.Header>
+        <div className="header__logo">Logo</div>
         <nav className="header__nav">
-            <li className="header__link">
-                Menu
-            </li>
-            <li className="header__link">
-                Menu
-            </li>
-            <li className="header__link">
-                Menu
-            </li>
-            <li className="header__link">
-                Menu
-            </li>
+          <li className="header__link">Menu</li>
+          <li className="header__link">Menu</li>
+          <li className="header__link">Menu</li>
+          <li className="header__link">Menu</li>
         </nav>
-        
-    </S.Header>;
+      </S.Header>
+    );
   }
 }
--- src/styles/pages/header.js
+++ src/styles/pages/header.js
@@ -6,6 +6,8 @@ S.Header = styled.div`
   width: 100%;
   height: 100px;
   background-color: #f1f1f1;
+  .header__nav {
+  }
 `;
 
 export default S;
