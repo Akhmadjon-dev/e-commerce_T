a1d59f99bd2b9012cd0e39c32d45b06c801160a2
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-02-07 23:09:01 +0500

    som bugs

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import S from "../../styles/pages/header";
 
-import logo from "../../assets/img/logo.jpg";
-import {ReactComponent as Logo} from "../../assets/img/logo.svg";
+// import logo from "../../assets/img/logo.jpg";
 export default class Header extends Component {
   render() {
     return (
       <S.Header>
         <div className="header__logo">Logo</div>
-        <img src={logo} alt="logo" />
-        <Logo />
         <nav className="header__nav">
           <li className="header__link">Menu</li>
           <li className="header__link">Menu</li>
