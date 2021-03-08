98096748bbf916c57bdb3d644d04e4928ca823b5
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-03-08 19:11:19 +0500

    basic validation added form on submit

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,46 +3,65 @@ import { MdClose } from "react-icons/md";
 import { BiUser } from "react-icons/bi";
 import { FiPhone } from "react-icons/fi";
 import { VscKey } from "react-icons/vsc";
-import { Link } from "react-router-dom";
 import SignUp from "./SignUp";
 import { Signin } from "../style/index";
 
 export default class SignIn extends Component {
   state = {
     isShow: false,
-    data: { phone: "", password: "", file: "" },
+    data: { phone: "", password: "" },
+    errors: {
+      // phone: "Phone is required",
+      // password: "Password is required",
+    },
   };
 
   modalHandler = () => {
     this.setState((prevState) => ({ isShow: !prevState.isShow }));
   };
+
   inputHandler = (e) => {
     const { name, value } = e.target;
     this.setState((prevState) => ({
       ...prevState,
       data: { ...prevState.data, [name]: value },
     }));
-    console.log(name, value);
+
+    console.log(this.state.data);
+  };
+
+  validate = () => {
+    const errors = {};
+    const { data } = this.state;
+
+    if (data.password.trim() === "") errors.password = "Password is required";
+    if (data.phone.trim() === "") errors.phone = "Phone is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   formHandler = (e) => {
-    const { phone, password, file } = this.state.data;
     e.preventDefault();
-    let formData = new FormData();
-    formData.append("phone", phone);
-    formData.append("password", password);
-    formData.append("file", file);
 
-    // fetch
-    //   .post("/api/users", {
-    //     method: "POST",
-    //     body: formData,
-    //   })
-    //   .then((res) => {});
+    const errors = this.validate();
+
+    console.log(errors, "errors coming!!!!");
+
+    this.setState({ errors: errors || {} });
+
+    if (errors) return;
+
+    console.log("form submitted");
+
+    // let formData = new FormData();
+    // formData.append("phone", phone);
+    // formData.append("password", password);
+
+    // call  server
   };
 
   render() {
-    const { isShow, data } = this.state;
+    const { isShow, data, errors } = this.state;
     return (
       <Signin>
         {isShow ? (
@@ -59,29 +78,31 @@ export default class SignIn extends Component {
                 <FiPhone className="tel" />
                 <input
                   onChange={this.inputHandler}
-                  defaultValue={data.phone}
+                  value={data.phone}
                   autoFocus
                   name="phone"
                   type="tel"
                   placeholder="Телефон"
                 />
               </div>
+              <p>{errors.phone}</p>
               <div>
                 <VscKey className="kalit" />
                 <input
                   onChange={this.inputHandler}
-                  defaultValue={data.password}
+                  value={data.password}
                   name="password"
                   type="password"
                   placeholder="Пароль"
                 />
               </div>
+              <p>{errors.password}</p>
             </div>
-            <Link className="link">Забыли пароль?</Link>
+            <p className="link">Забыли пароль?</p>
             <button className="signin__button">Войти</button>
             <p className="signin__p">
               Впервые у нас?
-              <Link onClick={this.modalHandler}>Зарегистрироваться </Link>
+              <p onClick={this.modalHandler}>Зарегистрироваться </p>
             </p>
           </form>
         )}
